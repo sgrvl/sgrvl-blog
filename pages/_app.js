9cb6f89ae799953a9a9e89b9e3f5391c8f793a85
Simon Gravel <simgrvl@gmail.com>
2020-10-11 21:59:48 -0400

    startin design

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,29 @@
 // import App from 'next/app'
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import Header from "../components/Header";
 import "../Normalize.css";
 
 const GlobalStyle = createGlobalStyle`
+	body {
+		background: ${(props) => props.theme.body};
+		color: ${(props) => props.theme.text};
+	}
 `;
+
+const theme = {
+	body: "#222831",
+	text: "#ececec",
+	secondary: "#f2a365",
+	highlight: "#30475e",
+};
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<>
+		<ThemeProvider theme={theme}>
 			<GlobalStyle />
 			<Header />
 			<Component {...pageProps} />
-		</>
+		</ThemeProvider>
 	);
 }
 
--- pages/blog.js
+++ pages/blog.js
@@ -2,6 +2,22 @@ import React from "react";
 import Head from "next/head";
 import Link from "next/link";
 import fs from "fs";
+import styled from "styled-components";
+
+const Wrap = styled.ul`
+	max-width: 600px;
+	margin: 0 auto;
+
+	li {
+		margin-bottom: 0.5rem;
+	}
+
+	a {
+		color: ${(props) => props.theme.secondary};
+		text-decoration: none;
+		font-size: 18px;
+	}
+`;
 
 const Blog = ({ slugs }) => {
 	return (
@@ -10,15 +26,21 @@ const Blog = ({ slugs }) => {
 				<title>s.grvl | blog</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			{slugs.map((slug) => {
-				return (
-					<div key={slug}>
-						<Link href="/blog/[slug]" as={`/blog/${slug}`}>
-							<a>{slug}</a>
-						</Link>
-					</div>
-				);
-			})}
+			<Wrap>
+				{slugs.map((slug) => {
+					return (
+						<li key={slug}>
+							<Link href="/blog/[slug]" as={`/blog/${slug}`}>
+								<a>
+									{slug
+										.replace(/-/g, " ")
+										.replace(slug[0], slug[0].toUpperCase())}
+								</a>
+							</Link>
+						</li>
+					);
+				})}
+			</Wrap>
 		</>
 	);
 };
